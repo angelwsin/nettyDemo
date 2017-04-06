3dcf48768f2ba7eb2b34e373cfe3d92a31870b79
angel <angel>
2017-04-06 21:54:07 +0800

    protobuf

---
+++ nettyDemo/src/main/java/com/encode/decode/protobuf/SubReqServer.java
@@ -0,0 +1,51 @@
+package com.encode.decode.protobuf;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class SubReqServer {
+	
+	
+	
+	
+	
+	
+	 public void bind(String host,int port)throws Exception{
+		        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+		        NioEventLoopGroup workGroup =  new NioEventLoopGroup();
+		        
+		        try{
+		        	ServerBootstrap  boot = new ServerBootstrap();
+		        	boot.group(bossGroup, workGroup)
+		        	.channel(NioServerSocketChannel.class)
+		        	.option(ChannelOption.SO_BACKLOG, 100)
+		        	.handler(new LoggingHandler(LogLevel.INFO))
+		        	.childHandler(new SubChildHandler());
+		        	
+		        	ChannelFuture future = boot.bind(host, port).sync();
+		        	future.channel().closeFuture().sync();
+		        }finally{
+		        	bossGroup.shutdownGracefully();
+		        	workGroup.shutdownGracefully();
+		        }
+	 }
+	 
+	 
+	 class SubChildHandler  extends ChannelInitializer<SocketChannel>{
+
+		@Override
+		protected void initChannel(SocketChannel ch) throws Exception {
+			
+			ch.pipeline().addLast(handlers)
+		}
+		 
+	 }
+
+}
