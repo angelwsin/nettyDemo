ab0b5ce2e627fb0e4cb0331c9ac6e655737b39a9
angelwsin <angelwsin@163.com>
2017-04-07 16:04:49 +0800

    bytebuf

---
+++ nettyDemo/src/main/java/com/netty/byteBuf/ByteBufTest.java
@@ -0,0 +1,31 @@
+package com.netty.byteBuf;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+import java.nio.ByteBuffer;
+
+public class ByteBufTest {
+
+    
+    public static void main(String[] args) {
+        
+        //java ByteBuffer
+        
+        // ByteBuffer  的三大属性   limit option capacity
+        // 由写到读 要调用 flip()
+        ByteBuffer  buf  = ByteBuffer.allocate(1024);
+        
+        //netty 的 ByteBuf
+        ByteBuf  b = Unpooled.buffer(1024);
+        b.readBytes("netty".getBytes());
+        //读取位置
+        b.readerIndex();
+        //读完后清理
+        b.discardReadBytes();
+        //写位置
+        b.writerIndex();
+        
+        
+    }
+}
