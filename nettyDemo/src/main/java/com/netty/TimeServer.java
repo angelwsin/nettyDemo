9f6ef5a52f71e0772175e828a34f800221ffed6a
angelwsin <angelwsin@163.com>
2017-04-17 17:28:37 +0800

    study

--- nettyDemo/src/main/java/com/netty/TimeServer.java
+++ nettyDemo/src/main/java/com/netty/TimeServer.java
@@ -39,9 +39,9 @@ public class TimeServer extends CommCompont{
 			  .handler(new LoggingHandler(LogLevel.DEBUG))
 			  .childHandler(new ChildChannelHandler());
 			  
-			  //绑定端口 同步等待
+			  //绑定端口 同步等待   sync 调用 wait()
 			  ChannelFuture future =   boot.bind(port).sync();
-			  //等待服务端 端口关闭
+			  //等待服务端 端口关闭        sync 调用 wait() 等待 I/O线程通知  注意不要在handler中使用 会死锁
 			  future.channel().closeFuture().sync().addListener(new GenericFutureListener<Future<? super Void>>() {
 
                 public void operationComplete(Future<? super Void> future) throws Exception {
@@ -53,6 +53,7 @@ public class TimeServer extends CommCompont{
 		}finally {
 			//优雅的关闭
 			bossGroup.shutdownGracefully();
+			
 			workGroup.shutdownGracefully();
 		}
 		 
@@ -126,7 +127,7 @@ public class TimeServer extends CommCompont{
 	  *       pipeline.fireChannelRegistered()
 	  *       NioSocketChannel.isActive(){ch.isOpen() && ch.isConnected()}===>
 	  *       pipeline.fireChannelActive(){channel.read()}
-	  *       
+	  *       za
 	  *       
 	  *    
 	  *    
@@ -134,7 +135,50 @@ public class TimeServer extends CommCompont{
 	  *    2)ScheduledFutureTask 定时任务
 	  *    3)NioTask
 	  *    
-	  *      
+	  *    4.netty中的事件分为inbound和outbound
+	  *     1)inbound 通常i/o线程触发
+	  *     ChannelHandlerContext.fireChannelRegistered()  对应 channel注册到selector
+	  *     ChannelHandlerContext.fireChannelActive() tcp链路建立成功 分socket(connect)和serversocket(bind)
+	  *     ChannelHandlerContext.fireChannelRead(Object msg) 读事件   
+	  *     ChannelHandlerContext.fireChannelReadComplete()  读操作完成
+	  *     ChannelHandlerContext.fireExceptionCaught(Throwable cause) 异常事件
+	  *     ChannelHandlerContext.fireUserEventTriggered(Object event) 自定义事件
+	  *     ChannelHandlerContext fireChannelWritabilityChanged() channel的可写状态变化通知事件
+	  *     ChannelHandlerContext.fireChannelInactive() tcp连接关闭，连接不可用事件
+	  *     
+	  *     
+	  *     2)outbound 通常用户主动发出的i/o操作
+	  *     ChannelHandlerContext.bind(SocketAddress localAddress) 绑定本地地址serversocket(bind)
+	  *     ChannelHandlerContext.connect(SocketAddress remoteAddress) 客户端连接服务端事件socket(connect)
+	  *     ChannelHandlerContext.write(Object msg, ChannelPromise promise); 发送事件
+	  *     ChannelHandlerContext.flush() 刷新
+	  *     ChannelHandlerContext.read()  读事件
+	  *     ChannelHandlerContext.disconnect(ChannelPromise promise) 断开连接事件
+	  *     ChannelHandlerContext.close(ChannelPromise promise) 关闭当前channel
+	  *     
+	  *    5.拆包和组包（半包问题）
+	  *     解决
+	  *     1）固定长度
+	  *     2）回车换行分割 LineBasedFrameDecoder
+	  *     3）分割符区分整包消息 DelimiterBasedFrameDecoder
+	  *     4）指定长度标识整包消息 LengthFieldBasedFrameDecoder ，LengthFieldPrepender(对应 第一个字段为长度的，自动填充）
+	  *    6.handler
+	  *    1)系统  HeadHandler
+	  *    2)编码  ByteToMessageDecoder，MessageToMessageCodec，MessageToMessageEncoder
+	  *    3)其他系统  流量整型handler,读写超时handler,日志handler(LoggingHandler) 
+	  *    7.ChannelFuture Promise   异步添加 listenner
+	  *    
+	  *    ChannelFuture  (解决nio的异步） 开始一个I/O操作ChannelFuture
+	  *    1)有两种状态 
+	  *     uncompleted  非失败 非成功 非取消
+	  *     completed     失败      成功    取消
+	  *    异步I/O超时：1.tcp层面的超时 2.业务层面的超时 
+	  *    
+	  *    uncompleted             completed
+	  *    isDone = false          失败 isDone = true  cause not null
+	  *    isSuccess =false         成功isDone = true  isSuccess =true
+	  *    isCancelled =false       取消isDone = true  isCancelled =true
+	  *    cause  = null
 	  *      
 	  *      
 	  *      
